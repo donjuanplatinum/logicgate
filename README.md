ea3f3e175235328719ceec8e6a8b9eccfc1be135
donjuanplatinum <donjuan@lecturify.net>
2024-02-18 01:10:09 +0800

    cargo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # LogicGate
 [![crates.io](https://img.shields.io/crates/dr/logicgate)](https://crates.io/crates/logicgate)[![Version](https://img.shields.io/crates/v/logicgate)](https://crates.io/crates/logicgate)
-[![Matrix](https://img.shields.io/matrix/algori%3Amozilla.org?style=flat))](https://matrix.to/#/#algori:mozilla.org)
+[![Matrix](https://img.shields.io/matrix/algori%3Amozilla.org?style=flat)](https://matrix.to/#/#algori:mozilla.org)
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,36 @@
+
+
 ///逻辑门
 pub trait LogicGate {
-    fn get_result(&self) -> bool;
+    fn get_result(&self) -> Option<bool>;
 }
+
+
+
 ///与非门 全为true则为false
 /// # Examples
 /// ```
 /// use logicgate::Nand;
 /// use logicgate::LogicGate;
-/// let a = Nand{input1: true, input2: false};
-/// assert_eq!(a.get_result(),true)
+/// let a = Nand{input1: Some(true), input2: Some(false)};
+/// assert_eq!(a.get_result(),Some(true))
 /// ```
 #[derive(PartialEq)]
 pub struct Nand {
-    pub input1: bool,
-    pub input2: bool,
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
 }
 
 
 impl LogicGate for Nand {
 
-    fn get_result(&self) -> bool {
-	if self.input1 == true{
-	    if self.input2 == true {
-		return false;
+    fn get_result(&self) -> Option<bool> {
+	if self.input1 == Some(true){
+	    if self.input2 == Some(true) {
+		return Some(false);
 	    }
 	}
-	true
+	Some(true)
     }
 }
 
@@ -35,18 +40,18 @@ impl LogicGate for Nand {
 /// ```
 /// use logicgate::Not;
 /// use logicgate::LogicGate;
-/// let a = Not{input: true};
-/// let b = Not{input: false};
-/// assert_eq!(a.get_result(),false);
-/// assert_eq!(b.get_result(),true);
+/// let a = Not{input: Some(true)};
+/// let b = Not{input: Some(false)};
+/// assert_eq!(a.get_result(),Some(false));
+/// assert_eq!(b.get_result(),Some(true));
 /// ```
 pub struct Not {
-    pub input: bool,
+    pub input: Option<bool>,
 }
 
 
 impl LogicGate for Not {
-    fn get_result(&self) -> bool {
+    fn get_result(&self) -> Option<bool> {
 	let a = Nand{input1: self.input, input2: self.input};
 	a.get_result()
     }
@@ -57,18 +62,18 @@ impl LogicGate for Not {
 /// ```
 /// use logicgate::Or;
 /// use logicgate::LogicGate;
-/// let a = Or{input1: true,input2: false};
-/// let b = Or{input1: false,input2: false};
-/// assert_eq!(a.get_result(),true);
-/// assert_eq!(b.get_result(),false);
+/// let a = Or{input1: Some(true),input2: Some(false)};
+/// let b = Or{input1: Some(false),input2: Some(false)};
+/// assert_eq!(a.get_result(),Some(true));
+/// assert_eq!(b.get_result(),Some(false));
 /// ```
 pub struct Or {
-    pub input1: bool,
-    pub input2: bool,
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
 }
 
 impl LogicGate for Or {
-    fn get_result(&self) -> bool {
+    fn get_result(&self) -> Option<bool> {
 	let a = Nand{input1: self.input1, input2: self.input1};
 	let b = Nand{input1: self.input2, input2: self.input2};
 	let c = Nand{input1: a.get_result(),input2: b.get_result()};
@@ -76,24 +81,24 @@ impl LogicGate for Or {
     }
 }
 
-///或非门 有true则为false
+///或非门 有true则为Some(false)
 ///或非门由四个与非门组成
 /// # Examples
 /// ```
 /// use logicgate::Nor;
 /// use logicgate::LogicGate;
-/// let a = Nor{input1: true,input2: false};
-/// let b = Nor{input1: false,input2: false};
-/// assert_eq!(a.get_result(),false);
-/// assert_eq!(b.get_result(),true);
+/// let a = Nor{input1: Some(true),input2: Some(false)};
+/// let b = Nor{input1: Some(false),input2: Some(false)};
+/// assert_eq!(a.get_result(),Some(false));
+/// assert_eq!(b.get_result(),Some(true));
 /// ```
 pub struct Nor{
-    pub input1: bool,
-    pub input2: bool,
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
 }
 
 impl LogicGate for Nor {
-    fn get_result(&self) -> bool {
+    fn get_result(&self) -> Option<bool> {
 	let a = Nand{input1: self.input1, input2: self.input1};
 	let b = Nand{input1: self.input2, input2: self.input2};
 	let c = Nand{input1: a.get_result(),input2: b.get_result()};
@@ -107,18 +112,18 @@ impl LogicGate for Nor {
 /// ```
 /// use logicgate::And;
 /// use logicgate::LogicGate;
-/// let a = And{input1: true,input2: false};
-/// let b = And{input1: true,input2: true};
-/// assert_eq!(a.get_result(),false);
-/// assert_eq!(b.get_result(),true);
+/// let a = And{input1: Some(true),input2: Some(false)};
+/// let b = And{input1: Some(true),input2: Some(true)};
+/// assert_eq!(a.get_result(),Some(false));
+/// assert_eq!(b.get_result(),Some(true));
 /// ```
 pub struct And {
-    pub input1: bool,
-    pub input2: bool,
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
 }
 
 impl LogicGate for And {
-    fn get_result(&self) -> bool {
+    fn get_result(&self) -> Option<bool> {
 	let a = Nand{input1: self.input1, input2: self.input2};
 	let b = Nand{input1: a.get_result(), input2: a.get_result()};
 	b.get_result()
@@ -130,29 +135,29 @@ impl LogicGate for And {
 /// use logicgate::HighLevel;
 /// use logicgate::LogicGate;
 /// let a = HighLevel{};
-/// assert_eq!(a.get_result(),true);
+/// assert_eq!(a.get_result(),Some(true));
 /// ```
 pub struct HighLevel{
 }
 impl LogicGate for HighLevel {
-    fn get_result(&self) -> bool {
-	true
+    fn get_result(&self) -> Option<bool> {
+	Some(true)
     }
 }
 
-///低电平 输出false
+///低电平 输出Some(false)
 /// # Examples
 /// ```
 /// use logicgate::LowLevel;
 /// use logicgate::LogicGate;
 /// let a = LowLevel{};
-/// assert_eq!(a.get_result(),false);
+/// assert_eq!(a.get_result(),Some(false));
 /// ```
 pub struct LowLevel{
 }
 impl LogicGate for LowLevel {
-    fn get_result(&self) -> bool {
-	false
+    fn get_result(&self) -> Option<bool> {
+	Some(false)
     }
 }
 
@@ -161,19 +166,19 @@ impl LogicGate for LowLevel {
 /// ```
 /// use logicgate::Xor;
 /// use logicgate::LogicGate;
-/// let a = Xor{input1:true,input2:false};
-/// let b = Xor{input1:true,input2:true};
-/// let c = Xor{input1:false,input2:false};
-/// assert_eq!(a.get_result(),true);
-/// assert_eq!(b.get_result(),false);
-/// assert_eq!(c.get_result(),false);
+/// let a = Xor{input1:Some(true),input2:Some(false)};
+/// let b = Xor{input1:Some(true),input2:Some(true)};
+/// let c = Xor{input1:Some(false),input2:Some(false)};
+/// assert_eq!(a.get_result(),Some(true));
+/// assert_eq!(b.get_result(),Some(false));
+/// assert_eq!(c.get_result(),Some(false));
 /// ```
 pub struct Xor{
-    pub input1: bool,
-    pub input2: bool,
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
 }
 impl LogicGate for Xor{
-    fn get_result(&self) ->bool {
+    fn get_result(&self) ->Option<bool> {
 	let a:And = And{input1:self.input1,input2:self.input2};
 	let b:Nor = Nor{input1:self.input1,input2:self.input2};
 	let c:Nor = Nor{input1:a.get_result(),input2:b.get_result()};
@@ -187,23 +192,23 @@ impl LogicGate for Xor{
 /// ```
 /// use logicgate::ThreeOr;
 /// use logicgate::LogicGate;
-/// let a = ThreeOr{input1:true,input2:false,input3:false};
-/// let b = ThreeOr{input1:false,input2:true,input3:false};
-/// let c = ThreeOr{input1:false,input2:false,input3:true};
-/// let d = ThreeOr{input1:false,input2:false,input3:false};
-/// assert_eq!(a.get_result(),true);
-/// assert_eq!(b.get_result(),true);
-/// assert_eq!(c.get_result(),true);
-/// assert_eq!(d.get_result(),false);
+/// let a = ThreeOr{input1:Some(true),input2:Some(false),input3:Some(false)};
+/// let b = ThreeOr{input1:Some(false),input2:Some(true),input3:Some(false)};
+/// let c = ThreeOr{input1:Some(false),input2:Some(false),input3:Some(true)};
+/// let d = ThreeOr{input1:Some(false),input2:Some(false),input3:Some(false)};
+/// assert_eq!(a.get_result(),Some(true));
+/// assert_eq!(b.get_result(),Some(true));
+/// assert_eq!(c.get_result(),Some(true));
+/// assert_eq!(d.get_result(),Some(false));
 /// ```
 pub struct ThreeOr{
-    pub input1: bool,
-    pub input2: bool,
-    pub input3: bool,
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
+    pub input3: Option<bool>,
 }
 
 impl LogicGate for ThreeOr{
-    fn get_result(&self) ->bool {
+    fn get_result(&self) ->Option<bool> {
 	let a:Or = Or{input1:self.input1,input2:self.input2};
 	let b:Or = Or{input1:self.input2,input2:self.input3};
 	let c:Or = Or{input1:a.get_result(),input2:b.get_result()};
@@ -217,25 +222,25 @@ impl LogicGate for ThreeOr{
 /// ```
 /// use logicgate::ThreeAnd;
 /// use logicgate::LogicGate;
-/// let a = ThreeAnd{input1:true,input2:false,input3:false};
-/// let b = ThreeAnd{input1:false,input2:true,input3:false};
-/// let c = ThreeAnd{input1:false,input2:false,input3:true};
-/// let d = ThreeAnd{input1:false,input2:false,input3:false};
-/// let e = ThreeAnd{input1:true,input2:true,input3:true};
-/// assert_eq!(a.get_result(),false);
-/// assert_eq!(b.get_result(),false);
-/// assert_eq!(c.get_result(),false);
-/// assert_eq!(d.get_result(),false);
-/// assert_eq!(e.get_result(),true);
+/// let a = ThreeAnd{input1:Some(true),input2:Some(false),input3:Some(false)};
+/// let b = ThreeAnd{input1:Some(false),input2:Some(true),input3:Some(false)};
+/// let c = ThreeAnd{input1:Some(false),input2:Some(false),input3:Some(true)};
+/// let d = ThreeAnd{input1:Some(false),input2:Some(false),input3:Some(false)};
+/// let e = ThreeAnd{input1:Some(true),input2:Some(true),input3:Some(true)};
+/// assert_eq!(a.get_result(),Some(false));
+/// assert_eq!(b.get_result(),Some(false));
+/// assert_eq!(c.get_result(),Some(false));
+/// assert_eq!(d.get_result(),Some(false));
+/// assert_eq!(e.get_result(),Some(true));
 /// ```
 pub struct ThreeAnd{
-    pub input1: bool,
-    pub input2: bool,
-    pub input3: bool,
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
+    pub input3: Option<bool>,
 }
 
 impl LogicGate for ThreeAnd{
-    fn get_result(&self) ->bool {
+    fn get_result(&self) ->Option<bool> {
 	let a:And = And{input1:self.input1,input2:self.input2};
 	let b:And = And{input1:self.input2,input2:self.input3};
 	let c:And = And{input1:a.get_result(),input2:b.get_result()};
@@ -243,27 +248,126 @@ impl LogicGate for ThreeAnd{
     }
 }
 ///同或门
-///相同则为true
+///相同则为Some(true)
 /// # Examples
 /// ```
 /// use logicgate::Xnor;
 /// use logicgate::LogicGate;
-/// let a = Xnor{input1:true,input2:false};
-/// let b = Xnor{input1:true,input2:true};
-/// let c = Xnor{input1:false,input2:false};
-/// assert_eq!(a.get_result(),false);
-/// assert_eq!(b.get_result(),true);
-/// assert_eq!(c.get_result(),true);
+/// let a = Xnor{input1:Some(true),input2:Some(false)};
+/// let b = Xnor{input1:Some(true),input2:Some(true)};
+/// let c = Xnor{input1:Some(false),input2:Some(false)};
+/// assert_eq!(a.get_result(),Some(false));
+/// assert_eq!(b.get_result(),Some(true));
+/// assert_eq!(c.get_result(),Some(true));
 /// ```
 pub struct Xnor{
-    pub input1: bool,
-    pub input2: bool,
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
 }
 
 impl LogicGate for Xnor{
-    fn get_result(&self) ->bool {
+    fn get_result(&self) ->Option<bool> {
 	let a:Xor = Xor{input1:self.input1,input2:self.input2};
 	let b:Not = Not{input: a.get_result()};
 	b.get_result()
+    }
+}
+
+use std::time::Duration;
+///延迟线
+///Examples
+///```
+/// use logicgate::DelayLine;
+/// let a = DelayLine{delay: 20,input: Some(true)};
+/// 
+/// a.get_result();
+///```
+
+pub struct DelayLine {
+    /// 延迟时间，单位为毫秒
+    pub delay: u64,
+    pub input: Option<bool>,
+}
+
+impl DelayLine {
+    pub fn get_result(&self) -> Option<bool> {
+        std::thread::sleep(Duration::from_millis(self.delay));
+        self.input
+    }
+}
+
+///半加器
+///输出一个包含低位结果与进位的元组
+/// Examples
+///```
+///use logicgate::HalfAdder;
+///let a:HalfAdder = HalfAdder{input1:Some(true),input2:Some(false)};
+///assert_eq!(a.get_result(),(Some(true),Some(false)));
+///let a:HalfAdder = HalfAdder{input1:Some(true),input2:Some(true)};
+///assert_eq!(a.get_result(),(Some(false),Some(true)));
+///```
+pub struct HalfAdder {
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
+}
+
+
+
+impl HalfAdder{
+    ///返回(sum,carry)
+    pub fn get_result(&self) -> (Option<bool>,Option<bool>) {
+	let a:Xor = Xor{input1: self.input1,input2:self.input2};
+	let b:And = And{input1: self.input1,input2:self.input2};
+	(a.get_result(),b.get_result())
+    }
+}
+
+///全加器
+/// Examples
+///```
+///use logicgate::FullAdder;
+///let a:FullAdder = FullAdder{input1:Some(true),input2:Some(false),input3:Some(false)};
+///assert_eq!(a.get_result(),(Some(true),Some(false)));
+///let a:FullAdder = FullAdder{input1:Some(true),input2:Some(true),input3:Some(false)};
+///assert_eq!(a.get_result(),(Some(false),Some(true)));
+///let a:FullAdder = FullAdder{input1:Some(true),input2:Some(true),input3:Some(true)};
+///assert_eq!(a.get_result(),(Some(true),Some(true)));
+///```
+pub struct FullAdder {
+    pub input1: Option<bool>,
+    pub input2: Option<bool>,
+    pub input3: Option<bool>,
+}
+
+impl FullAdder {
+    ///返回(sum,carry)
+    pub fn get_result(&self) ->(Option<bool>,Option<bool>) {
+	let a: Xor = Xor{input1: self.input1, input2: self.input2};
+	let b: And = And{input1: self.input1, input2: self.input2};
+	let c: Xor = Xor{input1: a.get_result(), input2: self.input3};
+	let d: And = And{input1: c.get_result(), input2: self.input3};
+	let e: Or = Or{input1: b.get_result(),input2: d.get_result()};
+	(c.get_result(),e.get_result())
+    }
+}
+///一位开关
+/// Examples
+///```
+///use logicgate::Switch;
+///use crate::logicgate::LogicGate;
+/// let a:Switch = Switch{switch: Some(true),input:Some(false)};
+///assert_eq!(a.get_result(),Some(false));
+///```
+pub struct Switch {
+    pub switch: Option<bool>,
+    pub input: Option<bool>,
+}
+
+impl LogicGate for Switch {
+    fn get_result(&self) -> Option<bool> {
+	if self.switch == Some(true) {
+	    return self.input;
+	}
+	None
     }
 }
